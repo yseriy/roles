4c37435d81f0d68b6028f8e5e3ca8f3bb11a8088
Yaroslav Seryi <yseryi@voximplant.com>
2019-02-27 10:54:20 +0300

    Очистка кода.

--- src/test/java/local/ys/prototype/roles/SubUserFinderTest.java
+++ src/test/java/local/ys/prototype/roles/SubUserFinderTest.java
@@ -11,10 +11,10 @@ import java.sql.Statement;
 import java.util.Collection;
 import java.util.HashSet;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class SubUserFinderTest {
-        private Connection connection;
+    private Connection connection;
 
     @Before
     public void setup() throws SQLException {
@@ -49,27 +49,28 @@ public class SubUserFinderTest {
 
     @Test
     public void find() throws SQLException {
-        SubUser expectSubUser = new SubUser(1, 1, "test1", "pass1", "desc1");
+        Integer id = 1;
+        SubUser expectSubUser = new SubUser(id, 1, "test1", "pass1", "desc1");
 
         SubUserFinder subUserFinder = new SubUserFinder();
-        SubUser subUser = subUserFinder.find(connection, 1);
+        SubUser subUser = subUserFinder.find(connection, id);
 
         assertEquals(expectSubUser, subUser);
     }
 
     @Test
     public void findByAccountID() throws SQLException {
-        SubUser expectSubUser2 = new SubUser(2, 2, "test2", "pass2", "desc2");
-        SubUser expectSubUser3 = new SubUser(3, 2, "test3", "pass3", "desc3");
+        Integer accountID = 2;
+        SubUser expectSubUser2 = new SubUser(2, accountID, "test2", "pass2", "desc2");
+        SubUser expectSubUser3 = new SubUser(3, accountID, "test3", "pass3", "desc3");
 
         Collection<SubUser> expectedSubUsers = new HashSet<>();
         expectedSubUsers.add(expectSubUser2);
         expectedSubUsers.add(expectSubUser3);
 
         SubUserFinder subUserFinder = new SubUserFinder();
-        Collection<SubUser> subUsers = subUserFinder.findByAccountID(connection, 2);
+        Collection<SubUser> subUsers = subUserFinder.findByAccountID(connection, accountID);
 
         assertEquals(expectedSubUsers, subUsers);
     }
-
 }
